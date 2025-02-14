354a41f744814a64249dd32ef472bfae299ca45f
ac <ac@Robins-MacBook-Pro-2.local>
2025-02-14 00:39:47 -0500

    added pic, clean up

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.businesscard
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,8 +12,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material.icons.filled.Share
@@ -25,6 +27,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -40,7 +44,6 @@ class MainActivity : ComponentActivity() {
             BusinessCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -50,95 +53,110 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(modifier: Modifier = Modifier) {
     Column(modifier = modifier.fillMaxSize()) {
-        Column(
-            modifier = modifier
-                .fillMaxHeight(.6f)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            NameAndTitle("Ac Doe", "Android Software Engineer")
-        }
-        Column(
-            modifier = modifier
-                .weight(1f)
-                .fillMaxWidth()
-                .background(Color(0xFFDCEDC8)),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Contacts("123-456-7890", "socmed/ac", "ac@gmail.com")
-        }
+        NameAndJobTitle(
+            name = stringResource(R.string.full_name),
+            jobTitle = stringResource(R.string.job_title),
+            idPicture = painterResource(id = R.drawable.ac_girl)
+        )
+        ContactInfo(
+            phone = stringResource(R.string.phone_number),
+            phoneIcon = Icons.Default.Phone,
+            socMed = stringResource(R.string.socmed_link),
+            socMedShareIcon = Icons.Default.Share,
+            email = stringResource(R.string.email_address),
+            emailIcon = Icons.Default.Email
+        )
     }
 }
 
 @Composable
-fun NameAndTitle(name: String, jobTitle: String, modifier: Modifier = Modifier) {
+fun NameAndJobTitle(
+    name: String,
+    jobTitle: String,
+    idPicture: Painter,
+    modifier: Modifier = Modifier
+) {
     Column(
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxHeight(.6f)
+            .fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            //add an actual picture
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "",
-            contentScale = ContentScale.FillBounds,
-            modifier = modifier.padding(30.dp)
+            painter = idPicture,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(170.dp)
+                .padding(bottom = 10.dp)
+                .size(200.dp)
         )
-        Text(
-            text = name
-        )
-        Text(
-            text = jobTitle
-        )
+        Text(text = name)
+        Text(text = jobTitle)
     }
 }
 
 @Composable
-fun Contacts(phone: String, socMed: String, email: String, modifier: Modifier = Modifier) {
+fun ContactInfo(
+    phone: String,
+    phoneIcon: ImageVector,
+    socMed: String,
+    socMedShareIcon: ImageVector,
+    email: String,
+    emailIcon: ImageVector,
+    modifier: Modifier = Modifier
+) {
     Column(
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFDCEDC8)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-        //learn how to justifyAlign icons and texts
     ) {
         Row(modifier = modifier.padding(5.dp)) {
             Icon(
-                imageVector = Icons.Default.Phone,
-                contentDescription = "Mail Icon",
-                modifier = modifier.padding(end = 5.dp),
+                imageVector = phoneIcon,
+                contentDescription = stringResource(R.string.mail_icon),
+                modifier = modifier
+                    .padding(end = 5.dp)
+                    .offset(x = -4.dp),
                 tint = Color.Black
             )
             Text(text = phone)
         }
         Row(modifier = modifier.padding(5.dp)) {
             Icon(
-                imageVector = Icons.Default.Share,
-                contentDescription = "Share Icon",
-                modifier = modifier.padding(end = 5.dp),
+                imageVector = socMedShareIcon,
+                contentDescription = stringResource(R.string.share_icon),
+                modifier = modifier
+                    .padding(end = 5.dp)
+                    .offset(x = -14.dp),
                 tint = Color.Black
             )
-            Text(text = socMed)
+            Text(text = socMed,
+                modifier = modifier.offset(x=-6.dp))
         }
         Row(modifier = modifier.padding(5.dp)) {
             Icon(
-                imageVector = Icons.Default.MailOutline,
-                contentDescription = "Mail Icon",
+                imageVector = emailIcon,
+                contentDescription = stringResource(R.string.mail_icon),
                 modifier = modifier.padding(end = 5.dp),
                 tint = Color.Black
             )
-            Text(text = email)
+            Text(text = email,
+                modifier = modifier.offset(x=8.dp))
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
